3ac17d610c61e442191ddfc9c28e0190ff79f619
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-11 14:10:49 -0500

    Read From File

---
+++ ReadFromFile/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import util.ReadFile;
+
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        System.out.println("hi");
+
+        ReadFile.ReadMyFile();
+    }
+}
