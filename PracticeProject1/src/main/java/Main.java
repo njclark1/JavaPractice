f0ebe8b83628f92edb7ad41beb6233f4cf02466f
njclark1 <77775946+njclark1@users.noreply.github.com>
2022-02-11 15:58:23 -0500

    junk

--- PracticeProject1/src/main/java/Main.java
+++ PracticeProject1/src/main/java/Main.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        WriteToFile.WriteFile();
-        ReadFromFile.ReadFile();
+//        WriteToFile.WriteFile();
+//        ReadFromFile.ReadFile();
+        System.out.println("Hi");
     }
 }
