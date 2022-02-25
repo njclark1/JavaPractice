4b524c43aa35ca94aefe8eafcc22e5f9b3162ab1
Nicholas <nickj.clark14@gmail.com>
2022-02-25 00:22:37 -0500

    prac

--- RandomNumberGenerator/src/dev/nclark/Main.java
+++ RandomNumberGenerator/src/dev/nclark/Main.java
@@ -1,18 +1,24 @@
 package dev.nclark;
 
+import java.util.Comparator;
+import java.util.Map;
 import java.util.Random;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hi");
+        int x = Integer.parseInt(args[0]);
+        int y = Integer.parseInt(args[1]);
+        int z = Integer.parseInt(args[2]);
 
         Random random = new Random();
 
-        int x = 10;
-        for (int i = 0; i < x; i++) {
-            int randomInt = random.nextInt();
-            System.out.println(randomInt);
-        }
+        Map<Integer, Long> intMap = random.ints(x, y + 1, z + 1)
+                .boxed().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+
+        intMap.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.naturalOrder()))
+                .forEachOrdered(integerLongEntry -> System.out.println(integerLongEntry.getKey() + ": " + integerLongEntry.getValue()));
     }
 }
