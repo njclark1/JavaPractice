41f4060c475f1bde395b37e2b930dec866b049d4
Nicholas <nickj.clark14@gmail.com>
2022-02-24 23:31:15 -0500

    what

--- RandomNumberGenerator/src/dev/nclark/Main.java
+++ RandomNumberGenerator/src/dev/nclark/Main.java
@@ -1,8 +1,18 @@
 package dev.nclark;
 
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hi");
+
+        Random random = new Random();
+
+        int x = 10;
+        for (int i = 0; i < x; i++) {
+            int randomInt = random.nextInt();
+            System.out.println(randomInt);
+        }
     }
 }
